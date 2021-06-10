c13299c8d2a7c746cbdb77f109e7028a19c65e68
alexangria1992 <alex040892@gmail.com>
2021-06-10 20:49:45 +1200

    Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+
 function App() {
   return (
     <div>
       <Header />
+      <Home/>
     </div>
   );
 }
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from "./NavBar"
+const home = () => {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
+}
+
+export default home
